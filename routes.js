0c95ad1d74ab682d6d8137eb812547c4f3525c59
harrison-symes <symeshjb@gmail.com>
2018-01-26 12:34:07 +1300

    backing it up

--- routes.js
+++ routes.js
@@ -16,9 +16,9 @@ routes.get("/students", (req, res) => {
 
   db('students')
     .select()
-    .then((student) => {
-      console.log(student)
-      res.render('students-list', {student})
+    .then((students) => {
+      // console.log(students)
+      res.render('students-list', {students})
     })
     .catch((err) => {
       res.send('Problem loading the page' + err.message)
@@ -37,12 +37,31 @@ routes.get("/add-student", (req, res) => {
 routes.post("/add-student", (req, res) => {
   var body = req.body
 
-  console.log(body)
   db('students')
   .insert({first_name:body.firstname, last_name:body.lastname})
 
   res.redirect("/confirmationPage")
 })
+
+routes.get('/students/:id', (req, res) => {
+  var db = req.app.get('db')
+
+  var stud_id= req.params.id
+
+  db('students')
+  .select()
+  .where('students.id', stud_id)
+  .first()
+  .join('student_subjects', 'students.id', 'student_subjects.student_id')
+  .select('subject_id', 'id')
+  .join('scores', 'student_subjects.id', 'scores.student_subject_id')
+  .
+  .then((student)=> {
+    // console.log(student)
+  res.render('student-score', student)
+})
+})
+
 
 routes.get('/students/:id/add-score', (req, res) => {
   res.render('add-score', {})
